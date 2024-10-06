23c1c97a69f86b8b0f234792552005b025e176ea
Stephen <srr@routhouse.org>
2024-10-06 19:59:36 -0400

    Partially implement parse tree for / * + -, working on ()

--- parse-tree.js
+++ parse-tree.js
@@ -3,8 +3,15 @@ function main() {
     let tokens = []
     
     //test
-    tokens = tokenizeInput("34 + 555 * (2.699998 - 8) ^ 2 / 4");
-    tokens = tokenizeInput("44+82");
+    tokens = tokenizeInput("34 + 555 * (2.699998 - 8) + 2 / 4");
+    //tokens = tokenizeInput("44+82");
+
+    let p = new Parser(tokens);
+    console.log(p);
+    let root = p.generateParseTree(tokens);
+
+    console.log(root);
+    console.log(root.r);
 }
 
 
@@ -65,8 +72,7 @@ function tokenizeInput(input) {
     }
 
     //For debugging
-    console.log(tokens);
-    console.log(tokens.join(""));
+    return tokens;
 
 }
 
@@ -82,17 +88,99 @@ function isSingleCharOpp(elem) {
     return singleCharOpps.includes(elem);
 }
 
-function parseEquation(tree) {
+
+
+
+
+
+class Parser {
+    constructor(tokens) {
+        this.tokens = tokens;
+        this.curPos = 0;
+        this.curToken = this.tokens != null ? this.tokens[this.curPos] : null;
+    }
+
+    advanceToken() {
+        this.curPos++;
+        if (this.curPos < this.tokens.length) {
+            this.curToken = this.tokens[this.curPos];
+        }
+        else {
+            this.curToken = null;
+        }
+        
+    }
+    
+    generateParseTree() {
+        let node = this.parseAddSub();
+        return node;
+    }
+    
+    
+    
+    parseAddSub() {
+        let node = this.parseMultDiv();
+
+        if (this.curToken == "+" || this.curToken == "-") {
+            let opperator = this.curToken;
+            this.advanceToken();
+
+            let rightNode = this.parseMultDiv();
+
+            let newNode = new Node(opperator, node, rightNode);
+            node = newNode;
+        }
+
+        return node;
+    }
+    
+    parseMultDiv() {
+        let node = this.parseValParen();
+
+        if (this.curToken == "*" || this.curToken == "\\") {
+            let operation = this.curToken;
+            this.advanceToken();
+
+            let rightNode = this.parseValParen();
+
+            node = new Node(operation, node, rightNode);
+        }
+        return node;
+    }
+    
+    parseValParen() {
+        if (this.curToken == "(") {
+            this.advanceToken();
+            let node = this.generateParseTree();
+            if (this.curToken == ")") {
+                this.advanceToken();
+                return node; //maybe declare the node at the top level?
+            }
+            else {
+                return null;
+            }
+        }
+        else {
+            let node = new Node();
+            node.val = this.curToken;
+            this.advanceToken();
+            return node;
+        }
+    }
 
 }
 
-class parseTree {
+
+
+class Node {
     constructor(val = null, l = null, r = null) {
         this.val = val;
         this.l = l;
         this.r = r;
-    }
 
+        return this;
+    }
+    
 
 }
 
