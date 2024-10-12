742553664d9b4e89d4289610d23042aa1df45648
Stephen <srr@routhouse.org>
2024-10-11 21:41:05 -0400

    Add todo list

--- parse-tree.js
+++ parse-tree.js
@@ -1,4 +1,34 @@
-
+/*
+TODO:
+- Clean up parse functions a bit
+- Build in-order traversal
+- Make that traversal evaluate the tree
+- Build some test cases with the current tree structure
+- Fix any bugs
+- Build starter GUI
+- Deploy
+- More rigorous input validation
+- Add new features!
+    - Record history
+    - New operators.
+        - Sqrt
+            - May need a custom way to display it - "sqrt()" is a bit lame.
+        - Factorial
+        - Natural log
+        - e?
+        - Trig functions
+            - Keep an eye on degrees vs radians
+        - Absolute Value
+        - Some of these may be best handled by creating a new node type for unary 
+            operators.
+            - For that matter, consider making a dedicated terminal node type
+    - Make sure you can handle floating point math reasonably
+    - RPN mode
+        - Will be best served with a new, stack-based backend, which should be
+            much easier to implement
+        - Add highlighting to show which terms on the stack a given operation
+            will effect!
+*/
 function main() {
     let tokens = []
     
@@ -150,42 +180,6 @@ class Parser {
      * 
      * 
      */
-
-
-    /*
-    TODO:
-    - Clean up parse functions a bit
-    - Build in-order traversal
-    - Make that traversal evaluate the tree
-    - Build some test cases with the current tree structure
-    - Fix any bugs
-    - Build starter GUI
-    - Deploy
-    - More rigorous input validation
-    - Add new features!
-        - Record history
-        - New operators.
-            - Sqrt
-                - May need a custom way to display it - "sqrt()" is a bit lame.
-            - Factorial
-            - Natural log
-            - e?
-            - Trig functions
-                - Keep an eye on degrees vs radians
-            - Absolute Value
-            - Some of these may be best handled by creating a new node type for unary 
-              operators.
-                - For that matter, consider making a dedicated terminal node type
-        - Make sure you can handle floating point math reasonably
-        - RPN mode
-            - Will be best served with a new, stack-based backend, which should be
-              much easier to implement
-            - Add highlighting to show which terms on the stack a given operation
-              will effect!
-    
-        
-
-    */
 
     parseAddSub() {
         let node = this.parseMultDiv();
